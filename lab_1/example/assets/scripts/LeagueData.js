f6b1a8fba22addf6f950724f1ebfd04f172c92a3
Dmytro Rudnytskykh <rudnitskih@gmail.com>
2015-12-01 04:18:23 +0200

    example first lab

--- lab_1/example/assets/scripts/LeagueData.js
+++ lab_1/example/assets/scripts/LeagueData.js
@@ -1,6 +1,6 @@
 function LeagueData(){
 	var config = {
-		dataUrl: "../../football_data",
+		dataUrl: "http://rudnitskih.github.io/frontend-labs/lab_1/football_data/",
 		league: "en",
 		years: [
 			"2012-13",
@@ -10,15 +10,117 @@ function LeagueData(){
 		],
 	};
 
-	function getDataByYears(years){
-		var xhr = new XMLHttpRequest()
-		xhr.open('GET', 'phones.json', false);
+	function _getJsonUrl(season, clubs){
+		url = config.dataUrl + season + "/" + config.league +  ".1."; 
+		url += clubs ? "clubs." : "";
+		return url += "json"
 	}
 
+	function _getString(url, successHandler, errorHandler) {
+		if (localStorage[url]) {
+			successHandler && successHandler(localStorage[url]);
+		} else {
+			var xhr = typeof XMLHttpRequest != 'undefined'
+				? new XMLHttpRequest()
+				: new ActiveXObject('Microsoft.XMLHTTP');
+			xhr.open('get', url, false);
+			xhr.send();
+			status = xhr.status;
+			if (status == 200) {
+				data = xhr.responseText;
+				successHandler && successHandler(data);
+				localStorage.setItem(url, data)
+			} else {
+				errorHandler && errorHandler(status);
+			}
+		}
+	};
+
+	function getMatches(season){
+		var url = _getJsonUrl(season),
+			matches;
+
+		_getString(url, function(data){
+			matches = data;
+		}, function(status){
+			alert(status);
+		});
+
+		return JSON.parse(matches).rounds;
+	}
+
+	function getYears(){
+		return config.years;
+	} 
+
+	function getClubs(season) {
+		var url = _getJsonUrl(season, true),
+			clubs;
+
+
+		_getString(url, function(data){
+			clubs = data;
+		}, function(status){
+			alert(status);
+		});
+		return JSON.parse(clubs).clubs;
+	}
+
+	function getLeagueResults (matches, season) {
+		var teams = getClubs(season),
+			res = {};
+		if (Array.isArray(teams)) {
+			teams.forEach(function (team) {
+				res[team.code] = {
+					name: team.name,
+					win: 0,
+					loss: 0,
+					draw: 0,
+					matches: 0,
+					scoredGoals: 0,
+					goalsAgainst: 0,
+					diff: 0,
+					totalScore: 0
+				}
+			});
+			matches.forEach(function(round){
+				round.matches.forEach(function(match){
+					if (match.score1 > match.score2) {
+						res[match.team1.code].win += 1
+					} else if (match.score1 < match.score2) {
+						res[match.team2.code].win += 1
+					} else {
+						res[match.team1.code].draw += 1
+						res[match.team2.code].draw += 1
+					}
+					res[match.team1.code].matches += 1
+					res[match.team2.code].matches += 1
+					res[match.team1.code].scoredGoals += match.score1
+					res[match.team1.code].goalsAgainst += match.score2
+					res[match.team2.code].scoredGoals += match.score2
+					res[match.team2.code].goalsAgainst += match.score1
+
+				});
+			})
+			for (var key in res) {
+				team = res[key];
+				team.diff = team.scoredGoals - team.goalsAgainst;
+				team.totalScore = team.win * 3 + team.draw;
+				team.loss = team.matches - team.win - team.draw;
+			}
+		}
+		return res;
+	} 
+
 	return {
-		config: config
+		getYears: getYears,
+		getMatches: getMatches,
+		getClubs: getClubs,
+		getLeagueResults: getLeagueResults
 	}
 
 }
-
+if (!window.app) {
+	window.app = {};
+}
 window.app.leagueData = new LeagueData();
--- lab_1/example/assets/scripts/project.js
+++ lab_1/example/assets/scripts/project.js
@@ -1,29 +1,110 @@
 (function() {
 	"use strict";
+	
+	window.app = window.app || {};
 
-	function LeagueData(){
-		var config = {
-			dataUrl: "../../football_data",
-			league: "en",
-			years: [
-				"2012-13",
-				"2013-14",
-				"2014-15",
-				"2015-16"
-			],
-		};
+	function renderSidebar() {
+		var seasons = window.app.leagueData.getYears(),
+			navSidebar = document.querySelector(".nav-sidebar"),
+			liTemplate = navSidebar.querySelector("script[name='nav-years']"),
+			navItems = "";
 
-		function getDataByYears(years){
-			var xhr = new XMLHttpRequest()
-			xhr.open('GET', 'phones.json', false);
+		if (Array.isArray(seasons) && liTemplate){
+			liTemplate = liTemplate.innerHTML;
+			seasons.forEach(function(season, i){
+				navItems += liTemplate.replace('<%=years%>', season); 
+			})
 		}
 
-		return {
-			config: config
+		navSidebar.innerHTML = navItems;
+		navSidebar.addEventListener("click", sidebarClickHandler);
+		setPickedSeason();
+	}
+
+	function sidebarClickHandler(e){
+		e = e || Event;
+		var target = e.target;
+		while (target !== this) {
+			if (target.classList.contains("nav-sidebar-item")) {
+				setPickedSeason(target.outerText.trim());
+			}
+			target = target.parentNode;
 		}
 
 	}
 
-	window.leagueData = new LeagueData();
+	function setPickedSeason(season){
+		var seasons = window.app.leagueData.getYears(),
+			i;
+		if (!season) {
+			season = localStorage["season"];
+			if (!season) {
+				season = seasons[0]
+			}
+		};
+		i = seasons.indexOf(season);
+		if (season && i > -1) {
+			var items = document.querySelectorAll(".nav-sidebar-item");
+			localStorage.setItem("season", season);
+			window.app.currentSeason = season;
+			for (var j = 0; j < items.length; j++) {
+				var classList = items[j].classList;
+				j === i ? classList.add("active") : classList.remove("active");
+			};
+			
+			renderMatches(season);
+		}
+	}
+	function renderMatches (season) {
+		var matches = window.app.leagueData.getMatches(season),
+			matchesBody = document.getElementsByClassName("matches-results__body")[0],	
+			matchesHtml = "",
+			res = "";
+
+		if (Array.isArray(matches)) {
+			matches.forEach(function(round){
+				matchesHtml += "<tr class='mathes-results__round-name'><td colspan='3'>" + round.name + "</td></tr>";
+				round.matches.forEach(function(match){
+					matchesHtml += "<tr>" +
+						"<td>" + match.date + "</td>" +
+						"<td>" + match.team1.name + " - " + match.team2.name + "</td>" +
+						"<td>" + match.score1 + " - " + match.score2 + "</td>" +
+					"</tr>";
+				});
+			})
+			matchesBody.innerHTML = matchesHtml;
+			window.app.currentRes = window.app.leagueData.getLeagueResults(matches, season);
+			renderChampions();
+		}
+	}
+
+	function renderChampions () {
+		var key,
+			teamsArray = [],
+			length = 4,
+			i = 0,
+			team,
+			champions = document.getElementsByClassName("champions")[0],
+			championsInnerHtml = "",
+			championTemplate = document.querySelector("script[name='champion']").innerHTML;
+
+		for (key in window.app.currentRes ){
+			teamsArray.push(window.app.currentRes[key])
+		}
+		teamsArray = teamsArray.sort(function(a, b){
+			return b.totalScore - a.totalScore;
+		})
+		while (i < length) {
+			team = teamsArray[i],
+			championsInnerHtml += championTemplate
+				.replace('<%=championName%>', team.name)
+				.replace('<%=championScore%>', team.totalScore)
+			i++;
+		}
+		champions.innerHTML = championsInnerHtml;
+		
+	};
+
+	renderSidebar()
 	
 })();
